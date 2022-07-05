8126fe29b0fd840bb7fda1810b8fd43c4c3df6e8
Jossic <jossic.lapierre@marvin-bot.com>
2022-07-05 13:46:08 +0200

    feat: Add login page

--- pizzapp/Models/Pizza.cs
+++ pizzapp/Models/Pizza.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace pizzapp.Models;
 
 public class Pizza
 {
     public int PizzaId { get; set; }
+    [Display(Name= "Nom")]
     public string Name { get; set; }
+    [Display(Name= "Prix")]
     public float Price { get; set; }
+    [Display(Name= "Vegan")]
     public bool Vegan { get; set; }
+    [Display(Name= "Ingrédients")]
     public string Ingredients { get; set; }
 }
